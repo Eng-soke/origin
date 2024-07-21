92e9e33d01510b4388ae4f956b0dbc9c76ddcaea
Eng Soke <cabdicasiismaxamed273@gmail.com>
2024-07-21 09:14:26 +0300

    update

--- src/Components/DhamaanNotes.jsx
+++ src/Components/DhamaanNotes.jsx
@@ -7,7 +7,10 @@ function DhamaanNotes(){
     useEffect(()=>{
 
         const dhmaanNotes = JSON.parse(localStorage.getItem("data"))
-        setData(dhmaanNotes)
+        if(dhmaanNotes){
+
+            setData(dhmaanNotes)
+        }
 
         
     })
@@ -35,13 +38,17 @@ function DhamaanNotes(){
 
     return <div  className=" grid sm:grid-cols-[320px_320px_320px] grid-cols-[300px] gap-10 m-12 sm:m-20 ">
         {
-          data.map((note)=>{
+         data.length > 0 ? data.map((note)=>{
                 return <div id="box" className=" border-2 border-gray-700 w-full h-[180px] p-5">
                     <h1 className="text-2xl font-bold border-b-2 border-gray-200"> {note.title} </h1>
                     <p> {note.description} </p>
                     <button onClick={()=> handleDelete(note.id) } id="btn" className="bg-red-500 hidden text-white mt-16 py-1 rounded px-3 ">Delete</button>
                 </div>
             })
+
+
+            :
+            <p>there is notes</p>
         }
         
            
